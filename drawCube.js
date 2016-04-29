22a04b79b4e882f53e8f777ba3a57e9fcbd60e35
jankaVar <janavareckova@yahoo.com>
2016-04-29 20:50:02 +0200

    little cube added

--- drawCube.js
+++ drawCube.js
@@ -1,12 +1,43 @@
-function draw() {
+var cubeColor = ["#326056", "#2A3D4C", "#3B3C3E", "#461420","#8B6943", "#ECCD65"];
+// parameters - different sides colors
+function drawCube(front, right, top) {
+  // get the canvas element using the DOM
   var canvas = document.getElementById('cubeCanvas');
+  // Make sure we don't execute when canvas isn't supported
   if (canvas.getContext) {
+    // use getContext to use the canvas for drawing
     var ctx = canvas.getContext('2d');
+    var xBottom = 200;
+    var yBottom = 200;
+    var width = 150; //width and height are the same
 
-    ctx.fillRect(25,25,100,100);
-    ctx.clearRect(45,45,60,60);
-    ctx.strokeRect(50,50,50,50);
+    ctx.fillStyle = cubeColor[top];
+    ctx.beginPath();
+    ctx.moveTo(xBottom,yBottom - width);
+    ctx.lineTo(xBottom + width/2,yBottom - width*1.5);
+    ctx.lineTo(xBottom - width/2,yBottom - width*1.5);
+    ctx.lineTo(xBottom - width,yBottom - width);
+    ctx.lineTo(xBottom,yBottom);
+    ctx.closePath();
+    ctx.fill();
+
+    ctx.fillStyle = cubeColor[right];
+    ctx.beginPath();
+    ctx.moveTo(xBottom,yBottom);
+    ctx.lineTo(xBottom,yBottom - width);
+    ctx.lineTo(xBottom + width/2,yBottom - width*1.5);
+    ctx.lineTo(xBottom + width/2,yBottom - width*0.5);
+    ctx.lineTo(xBottom,yBottom);
+    ctx.closePath();
+    ctx.fill();
+
+    ctx.fillStyle = cubeColor[front];
+    ctx.fillRect(xBottom - width,yBottom - width,xBottom,yBottom);
+
+  } else {
+     alert('You need Safari or Firefox 1.5+ to see this demo.');
   }
 }
 
-draw();
+//which side of the cube
+drawCube(1, 2, 3);
