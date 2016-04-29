4f53b7cfe84142bcb4645a43b2fa2451fb154ea8
Patricia <patriciapaulsson@gmail.com>
2016-04-29 15:50:34 +0200

    addpartner & selectpicture methods not tested

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
 var Picture = function (path) {
 	this.path = path; // path to the image
 	this.solved = false; // if it has been solved and should not be displayed anymore
+	this.partner = null;
+
+	this.addPartner = function (picture){
+		this.partner = picture;
+	}
 };
 
 var Side = function (pictures) {
@@ -8,10 +13,23 @@ var Side = function (pictures) {
 };
 
 var Cube = function () {
-	this.selectedPicture = false;
+	this.selectedPicture = null;
 	// load images into array
 	// create sides and populate them with images
 	// set a side as currently displayed side
+
+	this.selectPicture = function (picture){
+		if(selectedPicture == null){
+			this.selectedPicture = picture;
+		}else if (picture == this.selectedPicture ){
+			//Set pair as solved:
+			picture.solved = true;
+			this.selectedPicture.solved = true;
+			//Hide pictures TODO
+			//Clear selection:
+			this.selectedPicture = null;
+		}
+	}
 };
 
 Cube.prototype.selectPicture = function (picture) {
@@ -22,5 +40,5 @@ Cube.prototype.selectPicture = function (picture) {
 }
 
 Cube.prototype.displaySide = function (side) {
-	
+
 }
