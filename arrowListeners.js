0b602e2af1bc40fdbb5954043b9f613483a28047
janka <janavareckova@yahoo.com>
2016-04-30 13:57:27 +0000

    small comment and css changes

---
+++ arrowListeners.js
@@ -0,0 +1,21 @@
+document.getElementById("arrowUp").addEventListener("click", arrowUp);
+document.getElementById("arrowDown").addEventListener("click", arrowDown);
+document.getElementById("arrowLeft").addEventListener("click", arrowLeft);
+document.getElementById("arrowRight").addEventListener("click", arrowRight);
+
+
+function arrowUp (){
+    console.log("you clicked arrow up");
+}
+
+function arrowDown (){
+    console.log("you clicked arrow down");
+}
+
+function arrowLeft (){
+    console.log("you clicked arrow left");
+}
+
+function arrowRight (){
+    console.log("you clicked arrow right");
+}
