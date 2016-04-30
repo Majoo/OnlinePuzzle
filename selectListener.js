7c58e240b59f3f6c40b666fcd5702380136c591c
Dominik Seitz <seitzdo@gmail.com>
2016-04-30 22:33:04 +0200

    selection of images by user

--- selectListener.js
+++ selectListener.js
@@ -1,8 +1,10 @@
 $('document').ready(function() {
-	$("#img0").click(function(eventObject) {
-		var element = $( this );
-    	var picIndex = element.attr("alt");
-    	console.log("user selected " + picIndex);
-    	cube.selectPicture(cube.getFrontSide().pictures[picIndex]); //which picture did he select? which pictures are displayed? is it a 1x1 or 2x2 cube?
-	});
+	for (i = 0; i < picturesPerSide; i++) {
+		$("#img" + i).click(function(eventObject) {
+			var element = $( this );
+	    	var picIndex = element.attr("alt");
+	    	console.log("user selected " + picIndex);
+	    	cube.selectPicture(cube.getFrontSide().pictures[picIndex]); //which picture did he select? which pictures are displayed? is it a 1x1 or 2x2 cube?
+		});
+    }
 });
