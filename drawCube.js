a0ebe9735717b2110b543b91e472f0880e64c774
jankaVar <janavareckova@yahoo.com>
2016-04-30 15:00:38 +0200

    little cube repair, canvas properties changed

--- drawCube.js
+++ drawCube.js
@@ -1,4 +1,4 @@
-var cubeColor = ["#326056", "#2A3D4C", "#3B3C3E", "#461420","#8B6943", "#ECCD65"];
+var cubeColor = ["#F4CF64", "#E5A373", "#D7AFA5", "#4F4140","#FFF6D3", "#FF8366"];
 // parameters @front, @right, @top are numbers which cube side it is  and according to this different color will be used for drawing a cube
 function drawCube(front, right, top) {
   // get the canvas element using the DOM
@@ -7,32 +7,34 @@ function drawCube(front, right, top) {
   if (canvas.getContext) {
     // use getContext to use the canvas for drawing
     var ctx = canvas.getContext('2d');
-    var xBottom = 500;
-    var yBottom = 500;
-    var width = 250; //width and height are the same
+    var xBottom = 150;
+    var yBottom = 150;
+    var width = 100; //width and height are the same
 
     ctx.fillStyle = cubeColor[top];
     ctx.beginPath();
-    ctx.moveTo(xBottom,yBottom - width);
+    ctx.moveTo(xBottom, yBottom - width);
     ctx.lineTo(xBottom + width/2,yBottom - width*1.5);
     ctx.lineTo(xBottom - width/2,yBottom - width*1.5);
     ctx.lineTo(xBottom - width,yBottom - width);
-    ctx.lineTo(xBottom,yBottom);
+    ctx.lineTo(xBottom, yBottom - width);
     ctx.closePath();
     ctx.fill();
+    //ctx.stroke();
 
     ctx.fillStyle = cubeColor[right];
     ctx.beginPath();
-    ctx.moveTo(xBottom,yBottom);
-    ctx.lineTo(xBottom,yBottom - width);
-    ctx.lineTo(xBottom + width/2,yBottom - width*1.5);
-    ctx.lineTo(xBottom + width/2,yBottom - width*0.5);
+    ctx.moveTo(xBottom, yBottom);
+    ctx.lineTo(xBottom, yBottom - width);
+    ctx.lineTo(xBottom + width/2, yBottom - width*1.5);
+    ctx.lineTo(xBottom + width/2, yBottom - width*0.5);
     ctx.lineTo(xBottom,yBottom);
     ctx.closePath();
     ctx.fill();
+    // ctx.stroke();
 
     ctx.fillStyle = cubeColor[front];
-    ctx.fillRect(xBottom - width,yBottom - width,xBottom,yBottom);
+    ctx.fillRect(xBottom - width, yBottom - width, width, width);
 
   } else {
      alert('You need Safari or Firefox 1.5+ to see this demo.');
@@ -40,4 +42,4 @@ function drawCube(front, right, top) {
 }
 
 //which side of the cube
-drawCube(1, 2, 3);
+drawCube(0, 4, 5);
