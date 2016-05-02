163a65e20574d628426a30adc2a9cf0a4d96e671
Patricia <patriciapaulsson@gmail.com>
2016-05-02 11:46:35 +0200

    timer added

---
+++ timer.js
@@ -0,0 +1,24 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            alert("Time's up, you lost. Start over?");
+            timer = duration;
+            init(1);
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    var fourMinutes = 60 * 4,
+        display = $('#timeLbl');
+    startTimer(fourMinutes, display);
+});
